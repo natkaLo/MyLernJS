845a5f9a0aee41177fc25262d091af6949830761
natkaLo <loschuknata@gmail.com>
2021-11-18 07:20:32 +0200

    udemy _2

---
+++ udemy/js/scriptModalWindows.js
@@ -0,0 +1,19 @@
+// alert("1");
+// const result = confirm("Are you here");
+// console.log(result);
+// const answer = prompt("Вам есть уже 18?", "18");
+// console.log(answer);// всегда будет тип данных строка в answer
+// console.log(typeof(answer));
+
+//приведение к number
+// const answer = +prompt("Вам есть уже 18?", "18");
+// console.log(typeof(answer));
+// console.log(answer + 5);
+
+const answers = [];
+answers[0] = prompt("Как Ваше имя", "natka");
+answers[1] = prompt("Как Ваша фамилия", "Лощук");
+answers[2] = prompt("Сколько Вам лет", "50");
+
+document.write(answers);//устаревший
+console.log(typeof(answers));//object
