452bb2da9dacb835f83fcf39dd572d6d5a3709cb
natkaLo <loschuknata@gmail.com>
2022-01-25 14:49:42 +0200

    HOC

--- udemy/REACT/got_games-app/src/components/itemList/itemList.js
+++ udemy/REACT/got_games-app/src/components/itemList/itemList.js
@@ -3,27 +3,11 @@ import './itemList.css';
 //import GotService from '../../services/gotService';  - нужен без паттернов
 import Spinner from '../spinner';
 import PropTypes from 'prop-types';
+//import GotService from '../../services/gotService'; //нужен для ф-ции обертки
 
-export default class ItemList extends Component {
+class ItemList extends Component {
 
-    state = {
-        itemList:null
-    }
-    componentDidMount(){
-        const {getData} = this.props; // передаем функцию в качестве пропсов с верхнего уровня (из characterPage.js  getData = {this.gotService.getAllCharacters})
-        getData()                      // вызов getData() т.е вызов this.gotService.getAllCharacters
-        .then((itemList) =>{
-            this.setState({
-                itemList
-            })
-            // if(itemList){
-            //     const{id} = itemList[0]; //вытащим из item переменную id
-            //     this.props.onItemSelected(id);
-            // }
-        })
-    }
-
-    renderItems(arr){
+   renderItems(arr){
         return arr.map((item)=>{
             const{id} = item; //вытащим из item переменную id
             const label = this.props.renderItem(item);// передали функцию в качестве пропсов с верхнего уровня (из characterPage.js renderItem = {(item) => item.name}). Отсюда передали в ф-цю item, ф-ция вернула name 
@@ -39,12 +23,8 @@ export default class ItemList extends Component {
     }
 
     render() {
-        const {itemList}= this.state;
-        if(!itemList){
-            return <Spinner/>
-        }
-
-        const items = this.renderItems(itemList);
+        const {data} = this.props;
+        const items = this.renderItems(data);
 
         return (
             <ul className="item-list list-group">
@@ -64,6 +44,102 @@ ItemList.propTypes = {
    
 }
 
+ //компоненты высшего порядка (Higher-Order Component, HOC)
+ //ф-ция обертка
+//  const f = () => {
+//      return ItemList;
+//  }
+//ф-ция обертка для безымянного класса с аргументами
+//View это компонент ( наш ItemList), getData -ф -ция, переданная в качестве аргумента
+const withData = (View, getData) => {
+    return class extends Component {
+        // componentDidMount(){
+        //     console.log(this.props);
+        // }
+        // render(){
+        //     return <h1>Hello</h1>
+        // }
+
+        state = {
+            data:null
+        }
+        componentDidMount(){
+            const {getData} = this.props;
+             getData()                      // вызов getData() 
+            .then((data) =>{
+                this.setState({
+                    data
+                })
+                // if(itemList){
+                //     const{id} = itemList[0]; //вытащим из item переменную id
+                //     this.props.onItemSelected(id);
+                // }
+            })
+        }
+        render(){
+            const {data}= this.state;
+            if(!data){
+                return <Spinner/>
+            }
+            return <View {...this.props} data = {data}/>
+        }
+    }
+}
+//экспорт ф-ции и одновременно ее вызов
+export default withData(ItemList);
+//const {getAllCharacters} = new GotService();
+ //export default withData(ItemList, getAllCharacters);
+
+ //без  обертки (без компоненты высшего порядка (Higher-Order Component, HOC))
+//  class ItemList extends Component {
+
+//     state = {
+//         itemList:null
+//     }
+//     componentDidMount(){
+//         const {getData} = this.props; // передаем функцию в качестве пропсов с верхнего уровня (из characterPage.js  getData = {this.gotService.getAllCharacters})
+//         getData()                      // вызов getData() т.е вызов this.gotService.getAllCharacters
+//         .then((itemList) =>{
+//             this.setState({
+//                 itemList
+//             })
+//             // if(itemList){
+//             //     const{id} = itemList[0]; //вытащим из item переменную id
+//             //     this.props.onItemSelected(id);
+//             // }
+//         })
+//     }
+
+//     renderItems(arr){
+//         return arr.map((item)=>{
+//             const{id} = item; //вытащим из item переменную id
+//             const label = this.props.renderItem(item);// передали функцию в качестве пропсов с верхнего уровня (из characterPage.js renderItem = {(item) => item.name}). Отсюда передали в ф-цю item, ф-ция вернула name 
+//             return(
+//                 <li 
+//                     key = {id}
+//                     className="list-group-item"
+//                     onClick={()=>this.props.onItemSelected(id)}>
+//                     {label}
+//                 </li>
+//             )
+//         })
+//     }
+
+//     render() {
+//         const {itemList}= this.state;
+//         if(!itemList){
+//             return <Spinner/>
+//         }
+
+//         const items = this.renderItems(itemList);
+
+//         return (
+//             <ul className="item-list list-group">
+//               {items}
+//             </ul>
+//         );
+//     }
+// }
 
 // без паттернов
 // export default class ItemList extends Component {
